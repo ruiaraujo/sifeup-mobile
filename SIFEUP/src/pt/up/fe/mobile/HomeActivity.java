1c42281c7bb48b4b274b702e0b5b1a09fa4984a0
Ângela Igreja <angel.igreja@gmail.com>
2011-05-13 16:59:50 +0000

    Misplaced folder.

---
+++ SIFEUP/src/pt/up/fe/mobile/HomeActivity.java
@@ -0,0 +1,12 @@
+package pt.up.fe.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
