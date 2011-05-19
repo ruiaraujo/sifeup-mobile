d31e3af17845a3a849b3f914713ff64c8e904fcc
André Nuclear <andrenuclear@gmail.com>
2011-05-19 18:23:19 +0000

    Exams JSONError.

--- SiFEUP Mobile/src/pt/up/fe/mobile/ui/ExamsActivity.java
+++ SiFEUP Mobile/src/pt/up/fe/mobile/ui/ExamsActivity.java
@@ -8,6 +8,8 @@ import java.net.MalformedURLException;
 import javax.net.ssl.HttpsURLConnection;
 
 import org.apache.http.util.ByteArrayBuffer;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import pt.up.fe.mobile.R;
 import pt.up.fe.mobile.service.SessionCookie;
@@ -30,8 +32,7 @@ public class ExamsActivity extends Activity {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.exams);
         
-		//description_refresh
-        // definir accao para o botao de recarregarExames
+        // define action for examsRefresh button
         findViewById(R.id.examsRefresh).setOnClickListener(new OnClickListener() {
 			
 			@Override
@@ -49,7 +50,7 @@ public class ExamsActivity extends Activity {
 	private class ExamsTask extends AsyncTask<String, Void, Boolean> {
 		
     	/*protected void onPreExecute (){
-    	 	// iniciar dialogo de busca de dados
+    	 	// initiate dialog
     		showDialog(DIALOG_CONNECTING);  
     	}*/
 
@@ -57,7 +58,6 @@ public class ExamsActivity extends Activity {
         	if ( result )
         	{
 				Log.e("Exames","success");
-				//startActivity(new Intent(LoginActivity.this, HomeActivity.class));
 				Toast.makeText(ExamsActivity.this, "Yeah!", Toast.LENGTH_LONG).show();
 				
 			}
@@ -65,7 +65,7 @@ public class ExamsActivity extends Activity {
 				Log.e("Exames","error");
 				Toast.makeText(ExamsActivity.this, "Fuck", Toast.LENGTH_LONG).show();
 			}
-        	// retirar dialogo de busca de dados
+        	// remove dialog
         	//removeDialog(DIALOG_CONNECTING);
         }
         
@@ -74,14 +74,14 @@ public class ExamsActivity extends Activity {
 			InputStream in = null;
 			String page = null;
 			
-			//apos o click, pede a informacao ao servidor
+			//after a click, fetches info from server
 			try {
-				// iniciar coneccao
+				// initiate connection
 				HttpsURLConnection connection = LoginActivity.getUncheckedConnection(SifeupAPI.getExamsUrl("070509170"));
 				connection.setRequestProperty("Cookie", SessionCookie.getInstance().getCookie());
 				connection.connect();
 				
-				// buscar conteudo da coneccao
+				// fetch connection content
 				in = connection.getInputStream();
 				BufferedInputStream bis = new BufferedInputStream(in);
 				ByteArrayBuffer baf = new ByteArrayBuffer(100);
@@ -96,26 +96,52 @@ public class ExamsActivity extends Activity {
 					baf.append(buffer, 0, read);
 				}
 				page = new String(baf.toByteArray());
-				//String conteudoConeccao = SifeupAPI.getContents(coneccao);
-				//if(conteudoConeccao==null)
-				//	return false;
+				bis.close();
+				in.close();
+				
 				Log.e("APPPPPPPP", page);
 				
-				// terminar coneccao
+				// close connection
 				connection.disconnect();
 				
-				
-				// tratar dados
-				//JSONObject jObject = new JSONObject(conteudoConeccao);
-				//return jObject.optBoolean("authenticated");
-				
-				return true;
+				// check error
+				if(JSONError(page))
+					return false;
+				else
+					return true; 
 			}
 			catch (MalformedURLException e) {e.printStackTrace();}
-			catch (IOException e) {e.printStackTrace();}
+			catch (IOException e) {e.printStackTrace();} 
+			catch (JSONException e) {e.printStackTrace();}
 			
 			return false;
 		}
+	}
+	
+	/** 
+	 * Prints error message on Log.e()
+	 * Returns true in case of a existing error.
+	 * 
+	 * @param page
+	 * @return boolean
+	 * @throws JSONException
+	 */
+	public static boolean JSONError(String page) throws JSONException{
+		JSONObject jObject = new JSONObject(page);
+		String erro = null;
+		String erro_msg = null;
+		
+		if(jObject.has("erro")){
+			erro = (String) jObject.get("erro");
+			Log.e("APPPPPPPPerro", erro);
+			if(erro.substring(0, 8).equals("Autoriza")){
+				erro_msg = (String) jObject.get("erro_msg");
+				Log.e("APPPPPPPPerro_msg", erro_msg);
+			}
+			return true;
+		}
+		
+		return false;
 	}
 
 }
