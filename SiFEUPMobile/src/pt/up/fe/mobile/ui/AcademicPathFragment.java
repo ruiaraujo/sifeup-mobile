7f7ef8ae7dab56cb9c4c4d8d20ffddb203d23bb7
André Nuclear <andrenuclear@gmail.com>
2011-06-02 18:42:08 +0000

    Updated AcademicPath.

--- SiFEUPMobile/src/pt/up/fe/mobile/ui/AcademicPathFragment.java
+++ SiFEUPMobile/src/pt/up/fe/mobile/ui/AcademicPathFragment.java
@@ -1,9 +1,82 @@
 package pt.up.fe.mobile.ui;
 
+import org.json.JSONException;
+
+import pt.up.fe.mobile.R;
+import pt.up.fe.mobile.service.SessionManager;
+import pt.up.fe.mobile.service.SifeupAPI;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.widget.Toast;
 
 public class AcademicPathFragment extends ListFragment {
 
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //AnalyticsUtils.getInstance(getActivity()).trackPageView("/Exams");
+        new AcademicPathTask().execute();
+
+    }
 	
+	private class AcademicPathTask extends AsyncTask<Void, Void, String> {
+		
+		protected void onPreExecute (){
+    		if ( getActivity() != null ) 
+    			getActivity().showDialog(BaseActivity.DIALOG_FETCHING);  
+    	}
+
+        protected void onPostExecute(String result) {
+        	if ( !result.equals("") )
+        	{
+				Log.e("AcademicPath","success");
+    		}
+			else{	
+				Log.e("AcademicPath","error");
+				if ( getActivity() != null ) 
+				{
+					getActivity().removeDialog(BaseActivity.DIALOG_FETCHING);
+					Toast.makeText(getActivity(), getString(R.string.toast_auth_error), Toast.LENGTH_LONG).show();
+					((BaseActivity)getActivity()).goLogin(true);
+					return;
+				}
+			}
+        	if ( getActivity() != null ) 
+        		getActivity().removeDialog(BaseActivity.DIALOG_FETCHING);
+        } 
+		
+		@Override
+		protected String doInBackground(Void... theVoid) {
+			/*
+			String page = "";
+		  	try {
+	    			page = SifeupAPI.getExamsReply(
+								SessionManager.getInstance().getLoginCode());
+	    		if(	SifeupAPI.JSONError(page))
+	    		{
+		    		 return "";
+	    		}
+				
+	    		JSONAcademicPath(page);
+	    		
+				return "Sucess";
+				
+			} catch (JSONException e) {
+				if ( getActivity() != null ) 
+					Toast.makeText(getActivity(), "F*** JSON", Toast.LENGTH_LONG).show();
+				e.printStackTrace();
+			}
+			*/
+			return "";
+		}
+		 
+	}
+	
+	private boolean JSONAcademicPath(String page) {
+		
+		return true;
+	}
 	
 }
