d7a414f5f6059efbd4c9a50ca6dd5ea81ad7c854
Ângela Igreja <angel.igreja@gmail.com>
2011-08-08 17:42:28 +0000

    Proof of concept for the UC inscription.

--- SiFEUPMobile/src/pt/up/fe/mobile/ui/studentservices/UCsInscriptionsFragment.java
+++ SiFEUPMobile/src/pt/up/fe/mobile/ui/studentservices/UCsInscriptionsFragment.java
@@ -23,6 +23,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
 import external.com.google.android.apps.iosched.util.AnalyticsUtils;
@@ -30,6 +31,7 @@ import external.com.google.android.apps.iosched.util.AnalyticsUtils;
 
 public class UCsInscriptionsFragment extends Fragment
 {
+	ListView ucList;
 	 @Override
     public void onCreate(Bundle savedInstanceState) 
  	{
@@ -40,11 +42,30 @@ public class UCsInscriptionsFragment extends Fragment
 	 public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	            Bundle savedInstanceState) 
 	 {
-		ViewGroup root = (ViewGroup) inflater.inflate(R.layout.list_ucs_inscriptions, null);
+		ViewGroup root = (ViewGroup) inflater.inflate(R.layout.uc_inscription, null);
+		ucList = (ListView) root.findViewById(R.id.uc_listview);
 		
+		//TODO: proof of concept
+		 String[] from = new String[] {"uc"};
+         int[] to = new int[] { R.id.uc_check };
+	         // prepare the list of all records
+         List<HashMap<String, String>> fillMaps = new ArrayList<HashMap<String, String>>();
+         
+         HashMap<String, String> map = new HashMap<String, String>();
+         map.put("uc", "Comp" );
+         fillMaps.add(map);
+         map = new HashMap<String, String>();
+         map.put("uc", "IELE" );
+         fillMaps.add(map);
+         map = new HashMap<String, String>();
+         map.put("uc", "EIND" );
+         fillMaps.add(map);
+         // fill in the grid_item layout
+         SimpleAdapter adapter = new SimpleAdapter(getActivity(), fillMaps, R.layout.list_item_ucs_inscriptions, from, to);
+         ucList.setAdapter(adapter);
 		return  root;
 	
-	  } 
+	 } 
 	    
 	      
 }
