3ccbcb5b13241f282a0df70ff6fcefc21f9e404d
Ângela Igreja <angel.igreja@gmail.com>
2011-08-10 12:19:16 +0000

    Initial work on Lunch Menu.

---
+++ SiFEUPMobile/src/pt/up/fe/mobile/ui/lunchmenu/LunchMenuActivity.java
@@ -0,0 +1,32 @@
+package pt.up.fe.mobile.ui.lunchmenu;
+
+
+import pt.up.fe.mobile.ui.BaseSinglePaneActivity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+
+/**
+ * Lunch Menu Activity
+ * @author Ângela Igreja
+ */
+public class LunchMenuActivity extends BaseSinglePaneActivity 
+{
+	
+	@Override
+    protected Fragment onCreatePane() 
+	{
+		LunchMenuFragment lunchMenu =new LunchMenuFragment();
+	
+        return lunchMenu;
+    }
+	
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) 
+    {
+        super.onPostCreate(savedInstanceState);
+
+        getActivityHelper().setupSubActivity();
+    } 
+}
+
---
+++ SiFEUPMobile/src/pt/up/fe/mobile/ui/lunchmenu/LunchMenuFragment.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pt.up.fe.mobile.ui.lunchmenu;
+
+
+
+import pt.up.fe.mobile.R;
+import external.com.google.android.apps.iosched.util.AnalyticsUtils;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Lunch Menu Fragment
+ * @author Ângela Igreja
+ */
+public class LunchMenuFragment extends Fragment 
+{
+	
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+	    AnalyticsUtils.getInstance(getActivity()).trackPageView("/Lunch Menu");
+	}
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	            Bundle savedInstanceState) 
+	{
+		ViewGroup root = (ViewGroup) inflater.inflate(R.layout.uc_inscription, null);
+		
+		return  root;
+	} 	      
+}
