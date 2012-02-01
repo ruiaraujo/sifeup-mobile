9ee64a43580955a690424b7c0716a9d86b6127b4
Rui Araújo <rui.araujo@ieee.org>
2012-02-01 00:03:10 +0000

    Add uber cool animation.

--- SiFEUPMobile/src/pt/up/fe/mobile/ui/BaseFragment.java
+++ SiFEUPMobile/src/pt/up/fe/mobile/ui/BaseFragment.java
@@ -1,11 +1,17 @@
 package pt.up.fe.mobile.ui;
 
 import pt.up.fe.mobile.R;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Interpolator;
 import android.widget.ViewSwitcher;
 
 /**
@@ -22,15 +28,43 @@ public class BaseFragment extends Fragment {
 		return switcher;
     }
     
-    
+    private Interpolator accelerator = new AccelerateInterpolator();
+    private Interpolator decelerator = new DecelerateInterpolator();
+    private void flipit() {
+        final View visibleList;
+        final View invisibleList;
+        if (switcher.getCurrentView() == switcher.getChildAt(0)) {
+            visibleList = switcher.getChildAt(0);
+            invisibleList = switcher.getChildAt(1);
+        } else {
+            invisibleList = switcher.getChildAt(0);
+            visibleList = switcher.getChildAt(1);
+        }
+        ObjectAnimator visToInvis = ObjectAnimator.ofFloat(visibleList, "rotationY", 0f, 90f);
+        visToInvis.setDuration(500);
+        visToInvis.setInterpolator(accelerator);
+        final ObjectAnimator invisToVis = ObjectAnimator.ofFloat(invisibleList, "rotationY",
+                -90f, 0f);
+        invisToVis.setDuration(500);
+        invisToVis.setInterpolator(decelerator);
+        visToInvis.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator anim) {
+                invisToVis.start();
+                switcher.showNext();
+            }
+        });
+        visToInvis.start();
+    }
+
     protected void showLoadingScreen(){
     	if ( switcher.getCurrentView() != switcher.getChildAt(0) ) 
-			switcher.showNext();
+    	    flipit();
     }
     
     protected void showMainScreen(){
     	if ( switcher.getCurrentView() != switcher.getChildAt(1) ) 
-			switcher.showNext();
+    	    flipit();
     }
     
     protected ViewGroup getParentContainer(){
