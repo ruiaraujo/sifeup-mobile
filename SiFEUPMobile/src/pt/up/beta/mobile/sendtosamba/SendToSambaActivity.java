c975ac9531614051e6c9ed5af7eaaab5e8e24f24
Ângela Igreja <angel.igreja@gmail.com>
2012-09-17 11:36:20 +0200

    Ask for credential if there are none configured.

--- SiFEUPMobile/src/pt/up/beta/mobile/sendtosamba/SendToSambaActivity.java
+++ SiFEUPMobile/src/pt/up/beta/mobile/sendtosamba/SendToSambaActivity.java
@@ -1,11 +1,18 @@
 package pt.up.beta.mobile.sendtosamba;
 
+import pt.up.beta.mobile.Constants;
 import pt.up.beta.mobile.R;
+import pt.up.beta.mobile.authenticator.AuthenticatorActivity;
 import pt.up.beta.mobile.sifeup.AccountUtils;
-
+import pt.up.beta.mobile.ui.LauncherActivity;
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.widget.Toast;
 
@@ -22,12 +29,48 @@ public class SendToSambaActivity extends Activity {
 			i.putExtra(UploaderService.USERNAME_KEY, user);
 			i.putExtra(UploaderService.PASSWORD_KEY, pass);
 			startService(i);
+			finish();
 		} else {
+			startActivityForResult(
+					new Intent(this, AuthenticatorActivity.class), 0);
+		}
+	}
+	
+
+	@SuppressLint("CommitPrefEdits")
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == RESULT_OK) {
+			final AccountManager accountManager = AccountManager.get(getApplicationContext());
+			Account[] accounts = accountManager
+					.getAccountsByType(Constants.ACCOUNT_TYPE);
+			if ( accounts.length == 0 ){
+				finish();
+				return;
+			}
+			SharedPreferences loginSettings = PreferenceManager
+					.getDefaultSharedPreferences(getApplicationContext());
+			final SharedPreferences.Editor editor = loginSettings.edit();
+			
+			editor.putString(LauncherActivity.PREF_ACTIVE_USER, accounts[0].name);
+			new Thread(new Runnable() {
+				public void run() {
+					editor.commit();
+				}
+			}).start();
+			Intent i = new Intent(this, UploaderService.class);
+			i.replaceExtras(getIntent());
+			i.putExtra(UploaderService.USERNAME_KEY,  accounts[0].name);
+			i.putExtra(UploaderService.PASSWORD_KEY, accountManager.getPassword( accounts[0]));
+			startService(i);
+			finish();
+		}
+
+		if (resultCode == RESULT_CANCELED) {
 			Toast.makeText(this,
 					R.string.notification_uploader_error_credential,
 					Toast.LENGTH_SHORT).show();
+
 		}
-		finish();
 	}
 
 }
