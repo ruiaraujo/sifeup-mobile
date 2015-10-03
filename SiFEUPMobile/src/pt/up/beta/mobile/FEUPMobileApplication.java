06d60d3011f48727ea4fb26c50af58c06089e92f
gtugablue <gtugablue@gmail.com>
2015-10-03 19:56:28 +0100

    Disabled strict mode death penalty.

--- SiFEUPMobile/src/pt/up/beta/mobile/FEUPMobileApplication.java
+++ SiFEUPMobile/src/pt/up/beta/mobile/FEUPMobileApplication.java
@@ -6,7 +6,7 @@ import org.acra.annotation.ReportsCrashes;
 import android.app.Application;
 
 @ReportsCrashes(formKey = "", // will not be used
-	formUri = "http://paginas.fe.up.pt/~up201304143/SiFEUP%20Mobile/crashreport.php")
+formUri = "http://paginas.fe.up.pt/~up201304143/SiFEUP%20Mobile/crashreport.php")
 public class FEUPMobileApplication extends Application {
 	@Override
 	public void onCreate() {
